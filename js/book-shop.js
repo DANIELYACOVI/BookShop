43210dac0d29abdbe6acc96a4ef723781f144ce2
DANIELYACOVI <dannyyacovi6@gmail.com>
2024-02-02 18:50:58 +0200

    Design the table

--- js/book-shop.js
+++ js/book-shop.js
@@ -1,9 +1,34 @@
 'use strict'
 
-function onRemoveBook(bookId){
+var gBooks = [
+    { id: 'bg4J78', title: 'The adventures of Lori Ipsi', price: 120, imgUrl: 'lori-ipsi.jpg' },
+    { id: 'bg4J77', title: 'World Atlas', price: 300, imgUrl: 'lori-ipsi.jpg' },
+    { id: 'bg4J76', title: 'Zorba the Greek', price: 87, imgUrl: 'lori-ipsi.jpg' }
+]
+
+function onInit() {
+    renderBooks()
+}
+
+function renderBooks() {
+    const elBooks = document.querySelector('.book-list')
+    const strHtmls = gBooks.map(book => `<tr>
+    <td>${book.title}</td>
+    <td>${book.price}</td>
+    <td>
+        <button class="read">Read</button>
+        <button class="update">Update</button>
+        <button class="delete" onclick="onRemoveBook('${book.id}')">Delete</button>
+    </td>`)
+
+
+    elBooks.innerHTML = strHtmls.join('')
+}
+
+function onRemoveBook(bookId) {
     console.log('bookId:', bookId)
 }
 
-function onAddBook(){
+function onAddBook() {
     console.log('hi')
 }
